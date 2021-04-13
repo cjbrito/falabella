1765cdd3fe7740baaec6b41e32b00d247601baec
cjbrito <82467739+cjbrito@users.noreply.github.com>
2021-04-13 15:16:13 -0300

    Update version2.php

--- version2.php
+++ version2.php
@@ -2,9 +2,9 @@
 
 function getList() {
     $word = [
-        '101' => 'Falabella', 
-        '011' => 'IT', 
-        '111' => 'Integraciones'
+        '10' => 'Falabella', 
+        '01' => 'IT', 
+        '11' => 'Integraciones'
     ];
     $list = [];
     for ($i = 1; $i <= 100; $i++) {
@@ -12,9 +12,8 @@ function getList() {
         if ($i % 3 == 0 || $i % 5 == 0) {
             $w1 = ($i % 3 == 0) & 1;
             $w2 = ($i % 5 == 0) & 1;
-            $w3 = ($i % 3 == 0 || $i % 5 == 0) & 1;
             //echo $i, ' => ', $w1, ', ', $w2, ', ', $w3, ' =============> ', $word[$w1 . $w2 . $w3], '<br>';
-            $list[$i] = $word[$w1 . $w2 . $w3];
+            $list[$i] = $word[$w1 . $w2 ];
         }
     }
     return implode($list,",");
