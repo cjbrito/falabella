c1b1d2350f5e57fda34e30d50da760d1babe3886
cjbrito <82467739+cjbrito@users.noreply.github.com>
2021-04-13 16:31:47 -0300

    Update NumbersTest.php

--- test/NumbersTest.php
+++ test/NumbersTest.php
@@ -4,9 +4,23 @@ use PHPunit\Framework\TestCase;
 
 class NumbersTest extends TestCase
 {
-      public function testNumberReplace(){
-         require_once "Solucion.php";
-         $this->assertEquals('string',getList(100));
-      }
+
+   public function testNumberReplace(){
+      require_once "Solucion.php";
+      $this->assertEquals('string',getList(75));
+      $this->assertEquals('string',getList(18));
+      $this->assertEquals('string',getList(35));
+      $this->assertEquals('string',getList(57));
+      $this->assertEmpty("",getList(0));
+      $this->assertTrue(0<strlen(getList(100)));
+      
+   }
+
+    public function testNumberReplaceFail(){
+      require_once "Solucion.php";
+      $this->assertEmpty("",getList(100));
+      $this->assertTrue(0==strlen(getList(100)));
+      
+   }
 
 }
