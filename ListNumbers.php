c10cd764abc5243efca42531c8ca8d5dcd91e2f0
cjbrito <82467739+cjbrito@users.noreply.github.com>
2021-04-13 14:44:45 -0300

    Update ListNumbers.php

--- ListNumbers.php
+++ ListNumbers.php
@@ -1,5 +1,3 @@
-<?php
-
 function numberReplace($max_array){
    $output=[];
    for($i=1; $i<=$max_array; $i++){
@@ -12,16 +10,11 @@ function numberReplace($max_array){
 
    for($i=5; $i<=$max_array; $i+=5){
       $output[$i]="It";
-   }
-   for($i=3; $i<=$max_array; $i++){
       if($i%5==0 && $i%3==0){
               $output[$i]="INTEGRACIONES";
          continue;
       }
    }
+   
    return implode($output,",");
 }
-
-echo numberReplace(100);
-
-?>
