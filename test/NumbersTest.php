998e85c1959ce60602e23351b66c74a833d3bfd1
cjbrito <82467739+cjbrito@users.noreply.github.com>
2021-04-13 17:56:42 -0300

    Update NumbersTest.php

--- test/NumbersTest.php
+++ test/NumbersTest.php
@@ -4,22 +4,35 @@ use PHPunit\Framework\TestCase;
 
 class NumbersTest extends TestCase
 {
+   private $obj;
 
-   public function testNumberReplace(){
-      require_once "Solucion.php";
-      $this->assertEquals('string',getList(75));
-      $this->assertEquals('string',getList(18));
-      $this->assertEquals('string',getList(35));
-      $this->assertEquals('string',getList(57));
-      $this->assertEmpty("",getList(0));
-      $this->assertTrue(0<strlen(getList(100)));
-      
+   public function setUp():void{
+      require_once "Solution.php";
+      $this->obj= new Solution();
    }
 
-    public function testNumberReplaceFail(){
-      require_once "Solucion.php";
-      $this->assertEmpty("",getList(100));
-      $this->assertTrue(0==strlen(getList(100)));
+   public function testNumberWordReplace(){
+      $this->obj->getList(100);
+      $this->assertEquals('string', $this->obj->getStringList());
+      $this->assertEmpty("",$this->obj->getStringList());
+      $this->assertTrue(true, $this->obj->getWordsInList());
+
+      $this->obj->getList(18);
+      $this->assertTrue(true, $this->obj->getWordsInList());
+
+      $this->obj->getList(35);
+      $this->assertTrue(true, $this->obj->getWordsInList());
+
+      $this->obj->getList(57);
+      $this->assertTrue(true, $this->obj->getWordsInList());
+
+   }
+
+   public function testNumberReplaceFail(){
+      $this->obj->getList(14);
+      $this->assertTrue(false, $this->obj->getWordsInList());
+      $this->obj->getList(0);
+      $this->assertEmpty("",$this->obj->getStringList());
       
    }
 
